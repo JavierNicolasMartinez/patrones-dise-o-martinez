e2dba0c9fbbe234ac4b37cc61225440e150b8e71
Javier Nicolas Martinez <javierformosanm@gmail.com>
2026-08-27 15:02:51 -0300

    punto 2 terminado - metodo probado y testeado

--- diseño_factory_method.ts
+++ diseño_factory_method.ts
@@ -2,56 +2,65 @@ export {};
 
 type TipoEquipo = "Notebook" | "Desktop" | "Servidor";
 
+class EquipoFactory {
+  crearEquipo(
+    tipoEquipo: TipoEquipo,
+    nombre: string,
+    ram: string,
+    procesador: string,
+  ) {
+    switch (tipoEquipo) {
+      case "Notebook":
+        return new Notebook(tipoEquipo, nombre, ram, procesador);
+      case "Desktop":
+        return new Desktop(tipoEquipo, nombre, ram, procesador);
+      case "Servidor":
+        return new Servidor(tipoEquipo, nombre, ram, procesador);
+      default:
+        throw new Error();
+    }
+    // if (tipoEquipo == "Notebook") {
+    //   return new Notebook(tipoEquipo, nombre, ram, procesador);
+    // } else {
+    //   throw new Error();
+    // }
+    // return new Desktop(nombre, ram, procesador, tipoEquipo)
+  }
+}
+
 abstract class Equipo {
+  tipoEquipo: TipoEquipo;
+  nombre: string;
+  ram: string;
+  procesador: string;
+
   constructor(
-    protected nombre: string,
-    protected ram: string,
-    protected procesador: string
-  ) {}
+    tipoEquipo: TipoEquipo,
+    nombre: string,
+    ram: string,
+    procesador: string,
+  ) {
+    this.tipoEquipo = tipoEquipo;
+    this.nombre = nombre;
+    this.ram = ram;
+    this.procesador = procesador;
+  }
 
-  public abstract detalles(): string;
-
-  protected descripcion(): string {
-    return `Nombre: ${this.nombre}, RAM: ${this.ram}, Procesador: ${this.procesador}`;
+  detalles() {
+    console.log(
+      `Tipo: ${this.tipoEquipo}, Nombre: ${this.nombre}, RAM: ${this.ram}, Procesador: ${this.procesador}`,
+    );
   }
 }
 
 class Notebook extends Equipo {
-  public detalles(): string {
-    return `Tipo: Notebook, ${this.descripcion()}`;
-  }
+  super() {}
 }
-
 class Desktop extends Equipo {
-  public detalles(): string {
-    return `Tipo: Desktop, ${this.descripcion()}`;
-  }
+  super() {}
 }
-
 class Servidor extends Equipo {
-  public detalles(): string {
-    return `Tipo: Servidor, ${this.descripcion()}`;
-  }
-}
-
-class EquipoFactory {
-  public crearEquipo(
-    tipo: TipoEquipo,
-    nombre: string,
-    ram: string,
-    procesador: string
-  ): Equipo {
-    switch (tipo) {
-      case "Notebook":
-        return new Notebook(nombre, ram, procesador);
-      case "Desktop":
-        return new Desktop(nombre, ram, procesador);
-      case "Servidor":
-        return new Servidor(nombre, ram, procesador);
-      default:
-        throw new Error(`Tipo de equipo desconocido: ${tipo}`);
-    }
-  }
+  super() {}
 }
 
 const factory = new EquipoFactory();
